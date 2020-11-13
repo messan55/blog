c5da37c88a20f8fec06ce42c708f03e90f058c91
GNEMEGNA <messan.gnemegna@sogeti.com>
2020-11-13 18:00:19 +0100

    remove config

--- config/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  secret: "sdksnknckdnscnkscnkszldcd",
-  mongoURI: "mongodb+srv://messan:chiLWVr3o4Nghb7g@cluster0.hwgvc.mongodb.net/db-blog?retryWrites=true&w=majority",
-  cookieKey: 'redknjbhbjnjnjkjnlknnjkbhbhbbhbhbhacted',
-};
